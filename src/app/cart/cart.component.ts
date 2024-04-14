fc19e691d9e8d402766e5e22073aa0e2712a3b6d
Christopher <christopher.bondier@etu.univ-lyon1.fr>
2024-04-14 19:58:30 +0200

    form

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
-
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -9,8 +9,19 @@ import { CartService } from '../cart.service';
 export class CartComponent {
   constructor(
     private cartService: CartService,
+    private formBuilder: FormBuilder,
   ) { }
 
   items = this.cartService.getItems();
  
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
+  onSubmit(){
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
